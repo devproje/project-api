e93d84b62213ff79a4ec1b85f7e26f9cfdcbcd2e
Project_IO <me@projecttl.net>
2024-04-05 17:09:16 +0900

    feat: env support

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod api;
 mod routes;
 
+use std::env;
+use dotenv::dotenv;
 use log::{error, info};
 use crate::routes::{index, v1};
 use actix_web::{App, HttpServer, web};
@@ -12,8 +14,10 @@ static DEFAULT_PORT: u16 = 3000;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
     log4rs::init_file("log4rs.yml", Default::default())
-        .expect("Failed to initialize logging");
+        .expect("Failed to initialize log4rs");
 
     let route = || {
         App::new()
@@ -21,11 +25,14 @@ async fn main() -> std::io::Result<()> {
             .service(web::scope("/v1").configure(v1))
     };
 
-    let addr: &str = DEFAULT_ADDR;
-    let port: u16 = DEFAULT_PORT;
+    let addr = env::var("ADDR").unwrap_or_else(|_| DEFAULT_ADDR.to_string());
+    let port: u16 = match env::var("PORT") {
+        Ok(val) => val.parse().unwrap(),
+        Err(_) => DEFAULT_PORT
+    };
 
     let server = HttpServer::new(route);
-    match server.bind((addr, port)) {
+    match server.bind((addr.clone(), port)) {
         Ok(server) => {
             info!("Starting server at http://{}:{}", addr, port);
             server.run().await
